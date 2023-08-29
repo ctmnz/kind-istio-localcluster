2ee756e160d54f473b814718a7d846ef8d09d4f3
ctmnz <daniel.stoinov@gmail.com>
2023-08-29 20:11:51 +0300

    major changes

--- cluster-init.sh
+++ cluster-init.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
 # Create the local kubernetes cluster (using kind)
 kind create cluster --name=home-cluster --config=home-cluster.yaml
 
@@ -10,33 +8,48 @@ kind create cluster --name=home-cluster --config=home-cluster.yaml
 helm repo add istio https://istio-release.storage.googleapis.com/charts
 helm install istio-base istio/base -n istio-system --create-namespace
 helm install istiod istio/istiod -n istio-system --wait
+
 kubectl label ns default istio-injection=enabled --overwrite
 kubectl wait pods --for=condition=Ready -l app=istiod -n istio-system
 
+### Install jaeger
+#kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.18/samples/addons/jaeger.yaml
+kubectl apply -f ./services/jaeger.yaml
+
 ### Install and patch (for kind) istio-gateway
 
-helm install istio-gateway istio/gateway -n istio-system
-kubectl patch service istio-gateway -n istio-system --patch-file patches/gateway-svc-patch.yaml
+helm install istio-ingressgateway istio/gateway -n istio-system
+kubectl patch service/istio-ingressgateway -n istio-system --patch-file patches/gateway-svc-patch.yaml
 
-### Optional: Install example apps and gateway
-kubectl apply -f example-services.yaml
-kubectl apply -f example-gateway.yaml
+helm install istio-egressgateway istio/gateway -n istio-system
 
-echo "http://127.0.0.1/appA"
-echo "http://127.0.0.1/appB"
+### Install prometheus
+#kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.18/samples/addons/prometheus.yaml
+kubectl apply -f ./services/prometheus.yaml
 
-### Install monitoring
-
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm repo add grafana https://grafana.github.io/helm-charts
-helm install prometheus prometheus-community/prometheus -n monitoring --create-namespace
-# kubectl wait pods --for=condition=Ready -l app=prometheus -n monitoring --timeout=120s
-helm install grafana grafana/grafana -n monitoring -f "$SCRIPT_DIR"/grafana-value.yaml
-kubectl wait pods --for=condition=Ready -l app.kubernetes.io/instance=grafana -n monitoring --timeout=120s
+### Install grafana
+#kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.18/samples/addons/grafana.yaml
+kubectl apply -f ./services/grafana.yaml
 
 ### Install kiali
 
-helm install kiali-server kiali-server --repo https://kiali.org/helm-charts --set auth.strategy="anonymous" --set external_services.prometheus.url="http://prometheus-server.monitoring" -n istio-system
+#kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.18/samples/addons/kiali.yaml
+kubectl apply -f ./services/kiali.yaml
 
-echo "kubectl port-forward -n istio-system service/kiali 8080:20001"
+### Install cert-manager
+#kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.12.0/cert-manager.yaml
+kubectl apply -f ./services/cert-manager.yaml
+
+### Ingresses for the installed apps
+
+### http://grafana.127.0.0.1.nip.io/
+kubectl apply -f ./apps/grafana/gateway.yaml
+
+### http://kiali.127.0.0.1.nip.io/
+kubectl apply -f ./apps/kiali-gateway/gateway.yaml
+
+
+### Building the simple docker app (and upload it into the kind cluster)
+#docker build -t simple-app -f apps/src/simple-app-poc/Dockerfile apps/src/simple-app-poc
+
 
